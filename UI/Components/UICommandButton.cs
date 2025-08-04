975e926b32c23bf199b4c531e66c771298d1861b
eric-the-win <168ericthewin@gmail.com>
2025-08-04 21:11:03 +0800

    Lock Tooltip

--- UI/Components/UICommandButton.cs
+++ UI/Components/UICommandButton.cs
@@ -2,14 +2,20 @@
 using RTS_LEARN.Commands;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace RTS_LEARN.UI.Components
 {
     [RequireComponent(typeof(Button))]//force to add If not present
-    public class UICommandButton : MonoBehaviour, IUIElement<BaseCommand, UnityAction>
+    public class UICommandButton : MonoBehaviour, IUIElement<BaseCommand, UnityAction>, IPointerEnterHandler, IPointerExitHandler
     {
+        public UnityAction OnPointerEnter { get; set; }
+        public UnityAction OnPointerExit { get; set; }
+
+        [SerializeField] private BaseCommand command;
         [SerializeField] private Image icon;
+        [SerializeField] private Tooltip tooltip;
 
         private Button button;
 
@@ -24,6 +30,11 @@ namespace RTS_LEARN.UI.Components
             SetIcon(command.Icon);
             button.interactable = !command.IsLocked(new CommandContext());
             button.onClick.AddListener(onClick);
+
+            if (tooltip != null)
+            {
+                tooltip.SetText(command.name);
+            }
         }
 
         public void Disable()
@@ -31,6 +42,12 @@ namespace RTS_LEARN.UI.Components
             SetIcon(null);
             button.interactable = false;
             button.onClick.RemoveAllListeners();
+
+            if (tooltip != null)
+            {
+                tooltip.Hide();
+            }
+            CancelInvoke();
         }
 
         private void SetIcon(Sprite icon)
@@ -45,6 +62,31 @@ namespace RTS_LEARN.UI.Components
                 this.icon.enabled = true;
             }
 
+        }
+
+        void IPointerEnterHandler.OnPointerEnter(PointerEventData _)
+        {
+            // Invoke("ShowTooltip", 0.5f); //hardcoded string, prone to errors
+            Invoke(nameof(ShowTooltip), 0.5f);
+        }
+
+        private void ShowTooltip()
+        {
+            if (tooltip != null)
+            {
+                tooltip.Show();
+            }
+
+        }
+
+        void IPointerExitHandler.OnPointerExit(PointerEventData _)
+        {
+            if (tooltip != null)
+            {
+                tooltip.Hide();
+            }
+            CancelInvoke();//cancel all pending invocations
+            // CancelInvoke(nameof(ShowTooltip));
         }
     }
 }
